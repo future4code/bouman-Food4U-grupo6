aa9b965bddce6d935a49ddd8428df357e57c1b31
Caio-Braga <caioeduardobbsantos@gmail.com>
2020-03-16 17:39:37 -0300

    signupUC ainda incompleta, problema com o saltorrounds

---
+++ src/business/usecase/signupUC.ts
@@ -0,0 +1,29 @@
+import {v4} from "uuid";
+import {User} from "../entities/user";
+import { UserDB } from "../../data/userDB";
+import * as bcrypt from "bcrypt";
+
+interface SignupInput {
+    email: string
+    password: string
+}
+
+export class SignupUC {
+    async execute(input: SignupInput) {
+        try {
+            const userId = v4()
+
+        const hashPassword = await bcrypt.hash(input.password, saltOrRounds: 10)
+
+        const newUser = new User(userId, input.email, hashPassword)
+
+        const userDB = new UserDB()
+
+        await userDB.createUser(newUser)
+        } catch(e) {
+            console.log(e)
+            throw new Error("Problema ao criar usuário")
+        }
+        
+    }
+}
